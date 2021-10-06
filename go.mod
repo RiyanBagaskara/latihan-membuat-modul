5392b785bccd6302080f5429b78073b2a5fbafce
riyanbagas <riyanbagaskara@gmail.com>
2021-10-06 11:08:24 +0700

    major update say_hello dengan parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/RiyanBagaskara/latihan-membuat-modul
-
+//module github.com/RiyanBagaskara/latihan-membuat-modul	//Sebelum Major Update
+module github.com/RiyanBagaskara/latihan-membuat-modul/v2	//Setelah Major Update
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,9 @@
 package latihan_membuat_modul
 
-func SayHello() string{
-	return "Ini versi 1.5 Hello "
+//func SayHello() string{				//Contoh modul v1.5 sebelum Major Update
+//	return "Ini versi 1.5 Hello "
+//}
+
+func SayHello(name string) string{	//Contoh modul v2 setelah Major Update
+	return "Ini versi 2.0 Hello " +name
 }
